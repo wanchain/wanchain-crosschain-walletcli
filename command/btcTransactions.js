e4c09db79518afaec8614aafd0c584b8e5028e79
Zhang Lihua <zhang.lihua@wanglutech.com>
2018-08-23 21:11:31 +0800

    update btc

--- command/btcTransactions.js
+++ command/btcTransactions.js
@@ -93,7 +93,7 @@ vorpal
         let wanAddressList = [];
         try {
             wanAddressList = await ccUtil.getWanAccountsInfo(ccUtil.wanSender);
-
+            console.log("wanAddressList:", wanAddressList);
             print4log(sprintf("%46s %26s", "WAN address", "WBTC balance"));
             wanAddressList.forEach(function(wanAddress){
                 // web3.fromWei(wanAddress.balance),
@@ -214,6 +214,8 @@ vorpal
     .command('listStoreman', "get all storeman")
     .action(async function(args,callback) {
         let addressList;
+        let smgs = await ccUtil.getBtcSmgList(ccUtil.btcSender);
+        console.log(smgs);
 
         try{
             addressList = await btcUtil.getAddressList();
@@ -341,9 +343,10 @@ vorpal
 // lockwbtc
 vorpal
     .command('lockWbtc', "crosschain lockWbtc")
-    .action(function(args,callback){
+    .action(async function(args,callback){
         let self = this;
-
+        let smgs = await ccUtil.getBtcSmgList(ccUtil.btcSender);
+        console.log(smgs);
         let promise = this.prompt([
             {
                 type: 'input',
@@ -353,6 +356,11 @@ vorpal
             {
                 type: 'input',
                 name: 'wanAddress',
+                message: 'select the wan address you want to send wanchain transaction: '
+            },
+            {
+                type: 'input',
+                name: 'btcAddress',
                 message: 'Input the btc address you want to receive BitCoin: '
             },
             {
@@ -381,11 +389,32 @@ vorpal
 
         promise.then(async function(answers) {
             // Or promises!
-            print4log('wanAddress', answers.wanAddress);
-            print4log('btcAddress', answers.btcAddress);
-            print4log('amount', answers.amount);
-            print4log('rate', answers.rate);
-            print4log('password', answers.password);
+            print4log('answers:', answers);
+            print4log('typeof answers.gasLimit:', answers.gasLimit);
+
+            let wdTx = {};
+            wdTx.storemanGroup = storemanWanAddr;
+            wdTx.gas = Number(answers.gasLimit);
+            wdTx.gasPrice = Number(answers.gasPrice);
+            wdTx.passwd=answers.password;
+            wdTx.cross = '0x'+aliceHash160Addr;
+            wdTx.from = "0xbd100cf8286136659a7d63a38a154e28dbf3e0fd";
+            wdTx.amount = '0x'+wdValue.toString(16);
+            const txFeeRatio = 3;
+            wdTx.value = ccUtil.calculateLocWanFee(wdTx.amount,ccUtil.c2wRatio,  txFeeRatio);
+            console.log("wdTx.value: ",wdTx.value);
+            let x = btcUtil.generatePrivateKey().slice(2); // hex string without 0x
+            let hashx = bitcoin.crypto.sha256(Buffer.from(x, 'hex')).toString('hex');
+            wdTx.x = x;
+            console.log("wdTx:", wdTx);
+            console.log("wdtx hashx:", hashx);
+            let wdHash = await ccUtil.sendWanHash(ccUtil.wanSender, wdTx);
+            console.log("wdHash: ",wdHash);
+
+            // wait wallet tx confirm
+            await waitEventbyHashx('WBTC2BTCLock', config.HTLCWBTCInstAbi, '0x'+hashx);
+
+
 
             callback();
         });
